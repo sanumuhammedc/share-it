668f0e162e7158093f758328ac0c21ca3748e905
sanumuhammedc <sanumuhammed.c@gmail.com>
2023-03-17 22:49:08 +0530

    completed authentication api

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -24,5 +24,21 @@ export const register = async (req, res) => {
 //login function
 
 export const login = async (req, res) => {
-    
+    try {
+        const user = await userModel.findOne({email: req.body.email});
+        if(!user) {
+            res.status(404).json("user not found");
+            return;
+        }
+
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        if(!validPassword) {
+            res.status(400).json("wrong password");
+            return;
+        }
+
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(404).json({message: error.message})
+    }
 }
